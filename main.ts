ab4713e1f4e034170c2fa788a1d511e44ec06f89
Vesa Vänskä <vesa@vesavanska.com>
2022-02-26 11:08:19 +0200

    Add initial implementation

--- main.ts
+++ main.ts
@@ -1,137 +1,97 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin, Menu } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class MarkdownHeadingsPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: "increase-markdown-headings-command",
+			name: "Increase Markdown headings depth",
+			editorCheckCallback: (checking: boolean, editor: Editor) => {
+				if (checking) {
+					if (!editor.somethingSelected()) {
+						return false;
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
+
+				changeSelectionMarkdownHeadingsDepth(editor, 1);
+
+				return true;
+			},
+		});
+
+		this.addCommand({
+			id: "decrease-markdown-headings-command",
+			name: "Decrease Markdown headings depth",
+			editorCheckCallback: (checking: boolean, editor: Editor) => {
+				if (checking) {
+					if (!editor.somethingSelected()) {
+						return false;
+					}
+				}
+
+				changeSelectionMarkdownHeadingsDepth(editor, -1);
+
+				return true;
+			},
+		});
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu: Menu, editor: Editor) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Decrease Markdown headings depth")
+						.setIcon("left-arrow-with-tail")
+						.onClick(() => {
+							changeSelectionMarkdownHeadingsDepth(editor, -1);
+						});
+				});
+			})
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu: Menu, editor: Editor) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Increase Markdown headings depth")
+						.setIcon("right-arrow-with-tail")
+						.onClick(() => {
+							changeSelectionMarkdownHeadingsDepth(editor, 1);
+						});
+				});
+			})
+		);
+	}
+
+	onunload() {}
+}
+
+function changeSelectionMarkdownHeadingsDepth(editor: Editor, depth: number) {
+	const selection = editor.getSelection();
+	changeMarkdownHeadingsDepth(selection, depth).then((modified) => {
+		editor.replaceSelection(modified);
+	});
+}
+
+async function changeMarkdownHeadingsDepth(
+	source: string,
+	depth: number
+): Promise<string> {
+	return source
+		.split("\n")
+		.map((line) => {
+			const match = line.match(/^(#+) (.*)/);
+
+			if (match) {
+				let pounds;
+				if (depth > 0) {
+					pounds = match[1] + "#".repeat(depth);
+				} else {
+					pounds = match[1].slice(0, Math.max(match[1].length + depth, 1));
+				}
+
+				return `${pounds} ${match[2]}`;
+			} else {
+				return line;
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		})
+		.join("\n");
 }
